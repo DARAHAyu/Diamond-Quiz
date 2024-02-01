3b78ac2102a420d822e643e874caff71baa16189
DARAHAyu <harada11921027@icloud.com>
2024-02-01 14:04:46 +0900

    Add: quizdata.js

---
+++ client/src/quizdata.js
@@ -0,0 +1,21 @@
+const quizData = [
+    {
+      question: "日本はWBCで何回優勝している？",
+      choices: ["1回", "2回", "3回", "4回"],
+      answer: "3回"
+    },
+
+    {
+      question: "日本のプロ野球は何球団ある？",
+      choices: ["10球団", "11球団", "12球団", "13球団"],
+      answer: "12球団"
+    },
+
+    {
+      question: "日本のプロ野球は何リーグある？",
+      choices: ["1リーグ", "2リーグ", "3リーグ", "4リーグ"],
+      answer: "2リーグ"
+    },
+];
+
+export default quizData;
